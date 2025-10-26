ef8572526f384e01a7ca0dacc067dbb86a9198a5
Tchenzie <tguichette@gmail.com>
2025-10-26 19:48:57 -0400

    initial commit

---
+++ machine_vision/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'machine_vision'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tchenzie',
+    maintainer_email='tguichette@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'maze_follow = machine_vision.maze_follow:main'
+        ],
+    },
+)
