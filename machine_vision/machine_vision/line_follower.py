2ebd84eed6bf5725ac8f6b683f2fe2bd0e9f4d6e
Tchenzie <tguichette@gmail.com>
2025-11-01 19:04:00 -0400

    fixed line follower filter I think

--- machine_vision/machine_vision/line_follower.py
+++ machine_vision/machine_vision/line_follower.py
@@ -1,34 +1,32 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from neato2_interfaces.msg import Bump
-from enum import Enum
-import tty
-import select
-import sys
-import termios
+
 from time import sleep
 import cv2 as cv
 import numpy as np
+#publish the new image
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
 
 class LineFollow(Node):
     """Line following class"""
 
     def __init__(self):
         super().__init__("line_approach")
+        self.subscription = self.create_subscription(Image, '/camera/image_raw', 
+        self.image_callback,10)
+        #subscribe to rqt image then publish filtered one
+        self.publisher = self.create_publisher(Image, '/filtered_image_raw', 10)
 
-    def run_loop(self):
-        """Continuously filter the image"""
-        
 
-    def line_filter(self):
+    def line_filter(self,msg):
+        #get image then republish
         cap = cv.VideoCapture(0)
-    
         while(1):
         
             # Take each frame
-            _, frame = cap.read()
-        
+            frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')        
             # Convert BGR to HSV
             hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
         
@@ -42,12 +40,18 @@ class LineFollow(Node):
             # Bitwise-AND mask and original image
             res = cv.bitwise_and(frame,frame, mask= mask)
         
-            cv.imshow('frame',frame)
-            cv.imshow('mask',mask)
-            cv.imshow('res',res)
-            k = cv.waitKey(5) & 0xFF
-            if k == 27:
+            # Convert OpenCV image to ROS image message
+            ros_image = self.bridge.cv2_to_imgmsg(bw, encoding='mono8')
+            # Publish the filtered image
+            self.publisher.publish(ros_image)
+
+            # Optional: Display the filtered image
+            cv.imshow('Filtered Image', res)
+            if cv.waitKey(1) & 0xFF == ord('q'):
                 break
+
+            cap.release()
+        
         
         cv.destroyAllWindows()
 
