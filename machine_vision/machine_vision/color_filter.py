1058414719dc337fb7440554252bf1d0aaa9617b
Tchenzie <tguichette@gmail.com>
2025-11-05 20:48:41 -0500

    Maze logic added up to line follower point

--- machine_vision/machine_vision/color_filter.py
+++ machine_vision/machine_vision/color_filter.py
@@ -13,6 +13,7 @@ class maze_vision(Node):
     """Color filtering class"""
 
     def __init__(self):
+
         super().__init__("color_filter")
         self.bridge = CvBridge()
         self.subscription = self.create_subscription(Image, '/camera/image_raw', 
@@ -23,6 +24,12 @@ class maze_vision(Node):
 
     def image_callback(self,msg):
         
+        #Sensors and triggers in y,x
+        middle_sensor = 430,390
+        left_trigger = 430,230
+        right_trigger = 430,600
+        middle_trigger = 300,430
+
         # Take each frame
         frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')        
         # Convert BGR to HSV
@@ -37,7 +44,7 @@ class maze_vision(Node):
     
         # Bitwise-AND mask and original image
         res = cv.bitwise_and(frame,frame, mask= mask)
-    
+        
         # Convert OpenCV image to ROS image message
         ros_image = self.bridge.cv2_to_imgmsg(res, encoding='bgr8')
         # Publish the filtered image
@@ -46,9 +53,13 @@ class maze_vision(Node):
         # Optional: Display the filtered image
         cv.imshow('Filtered Image', res)
         cv.waitKey(1)
-    
-    
+        
+        print(res[430,390])
+        return res
+        
+
     cv.destroyAllWindows()
+    
 
 
 def main(args=None):
--- machine_vision/machine_vision/main_maze_follow.py
+++ machine_vision/machine_vision/main_maze_follow.py
@@ -2,26 +2,128 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Image
+import cv2 as cv
+from time import sleep
+import math
 
 class maze_follow(Node):
+    """A class executing all the maze logic"""
+        
     def __init__(self):
+        
         super().__init__("maze_follow")
         self.pub = self.create_publisher(Twist, "cmd_vel", 10)
-        self.run = True
 
         self.timer = self.create_timer(0.1, self.run_loop)
-    
-    def forward(self, speed):
+        self.subscription = self.create_subscription(
+            Image,
+            '/filtered_image_raw',
+            self.image_callback,
+            10
+        )
+
+    def drive(self, linear, angular):
+        """ Publishes cmd_vel to Twist topic to set Neato linear and angular velocities
+        Args: 
+            linear: linear velocity in m/s
+            angular: angular velocity in rad/s
+        """
         msg = Twist()
-        msg.linear.x = speed
+        msg.linear.x = linear
+        msg.angular.z = angular
         self.pub.publish(msg)
 
-    def run_loop(self):
+    def go_forward(self, distance):
+        """ Drives Neato in straight line based on specified velocity
+        Args:
+            distance: specified distance (meters)
         
+        """
+        forward_vel = 0.1
+        if self.bump_state == False:
+            self.drive(linear=forward_vel, angular=0.0)
+        sleep(distance/forward_vel)
+        self.drive(linear=0.0, angular=0.0)
+
+    def turn_right(self):
+        """Turns Neato 90 degrees based on set angular velocity """
+        ang_vel = 0.2
+        self.drive(linear= 0.0, angular= -ang_vel)
+        sleep(((math.pi/ang_vel)*(1/2)))
+        self.drive(linear=0.0, angular=0.0)
+
+    def turn_left(self):
+        """Turns Neato 90 degrees based on set angular velocity """
+        ang_vel = 0.2
+        self.drive(linear= 0.0, angular= ang_vel)
+        sleep(((math.pi/ang_vel)*(1/2)))
+        self.drive(linear=0.0, angular=0.0)
+    
+        
+
+    def run_loop(self,msg):
+        maze_stack = []
+        middle_sensor = 430,390
+        left_trigger = 430,230
+        right_trigger = 430,600
+        middle_trigger = 300,430
+
+        left_path = res[left_trigger][2]!=0
+        middle_path = res[middle_trigger][2]!=0
+        right_path = res[right_trigger][2]!=0
+
+        centered_on_tape = res[middle_sensor][2]!=0
+
+        
+
+        res = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        
+        intersection = (res[middle_trigger][2]!=0)+(res[left_trigger][2]!=0+(res[right_trigger][2]!=0) )>1
+        dead_end = res[middle_trigger][2]=0 and res[left_trigger][2]=0 and res[right_trigger][2]=0 
         """continuously check map and move accordingly"""
-        if self.run:
-            self.forward(0.2)
-            self.run = True
+
+        if not maze_stack:
+            #If any 2 directions are sensed at once
+            #Sensed: if red values are greater than 100. 0=blue,1=green, 2=red
+            if (intersection):
+                #Add all the sensed directions to the maze stack.
+                if (left_path):
+                    maze_stack.append("L")
+                if (middle_path):
+                    maze_stack.append("M")
+                if (right_path):
+                    maze_stack.append("R")
+            
+            #If Left or Right Path is seen  
+            elif (left_path or right_path) and not intersection:
+                #Move forward for some distance before turning
+                if right_path:
+                    self.go_forward(1)
+                    self.turn_right
+                    
+                if left_path:
+                    self.go_forward(1)
+                    self.turn_left
+
+            #If centered on tape go forward
+            elif centered_on_tape and not intersection:
+                self.forward(0.2)
+        #If there's a maze stack begin investigations
+        if maze_stack:  
+            if not dead_end:
+                if maze_stack:
+                    if maze_stack.top("L"):
+                        self.turn_left()
+                    if maze_stack.top("R"):
+                        self.turn_right()
+                    while not intersection:
+                        self.go_forward(1)
+        
+
+        # get res as a normal numpy OpenCV image
+        #cv.imshow('Received Filtered Image', res)
+        #cv.waitKey(1)
 
     
     #def maze_navigation(self,msg):
