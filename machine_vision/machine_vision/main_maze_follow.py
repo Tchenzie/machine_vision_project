51d904262d0ae32f28dd8a10cd73f2bc5bf125bc
Tchenzie <tguichette@gmail.com>
2025-11-05 23:32:17 -0500

    Finished Maze Follow Code Draft

--- machine_vision/machine_vision/main_maze_follow.py
+++ machine_vision/machine_vision/main_maze_follow.py
@@ -6,6 +6,7 @@ from sensor_msgs.msg import Image
 import cv2 as cv
 from time import sleep
 import math
+from cv_bridge import CvBridge
 
 class maze_follow(Node):
     """A class executing all the maze logic"""
@@ -14,14 +15,24 @@ class maze_follow(Node):
         
         super().__init__("maze_follow")
         self.pub = self.create_publisher(Twist, "cmd_vel", 10)
-
+        self.bridge = CvBridge()
         self.timer = self.create_timer(0.1, self.run_loop)
         self.subscription = self.create_subscription(
             Image,
             '/filtered_image_raw',
-            self.image_callback,
+            self.line_follower,
             10
         )
+        self.maze_stack = []
+        self.left_path = False
+        self.middle_path = False
+        self.right_path = False
+        self.intersection = False
+        self.dead_end = False
+        self.centered_on_tape = False
+        self.latest_image = None
+
+
 
     def drive(self, linear, angular):
         """ Publishes cmd_vel to Twist topic to set Neato linear and angular velocities
@@ -41,8 +52,8 @@ class maze_follow(Node):
         
         """
         forward_vel = 0.1
-        if self.bump_state == False:
-            self.drive(linear=forward_vel, angular=0.0)
+        
+        self.drive(linear=forward_vel, angular=0.0)
         sleep(distance/forward_vel)
         self.drive(linear=0.0, angular=0.0)
 
@@ -60,65 +71,128 @@ class maze_follow(Node):
         sleep(((math.pi/ang_vel)*(1/2)))
         self.drive(linear=0.0, angular=0.0)
     
-        
-
-    def run_loop(self,msg):
-        maze_stack = []
-        middle_sensor = 430,390
-        left_trigger = 430,230
-        right_trigger = 430,600
-        middle_trigger = 300,430
-
-        left_path = res[left_trigger][2]!=0
-        middle_path = res[middle_trigger][2]!=0
-        right_path = res[right_trigger][2]!=0
-
-        centered_on_tape = res[middle_sensor][2]!=0
-
-        
-
+    def line_follower_callback(self, msg):
+    # Convert ROS Image → OpenCV
+        self.latest_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
         res = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
         
-        intersection = (res[middle_trigger][2]!=0)+(res[left_trigger][2]!=0+(res[right_trigger][2]!=0) )>1
-        dead_end = res[middle_trigger][2]=0 and res[left_trigger][2]=0 and res[right_trigger][2]=0 
-        """continuously check map and move accordingly"""
+    def line_follower(self):
+        
+        if self.latest_image is None:
+         return  # no image yet
 
-        if not maze_stack:
+        res = self.latest_image
+        self.maze_stack = []
+        self.middle_sensor = 430,390
+        self.left_trigger = 430,230
+        self.right_trigger = 430,600
+        self.middle_trigger = 300,430
+
+        self.left_path = res[self.left_trigger][2]!=0
+        self.middle_path = res[self.middle_trigger][2]!=0
+        self.right_path = res[self.right_trigger][2]!=0
+
+        self.centered_on_tape = res[self.middle_sensor][2]!=0
+
+        self.intersection = (
+        (res[self.middle_trigger][2]!=0)+
+        (res[self.left_trigger][2]!=0) +
+        (res[self.right_trigger][2]!=0) 
+        )>1
+        self.dead_end = res[self.middle_trigger][2]==0 and (res[self.left_trigger][2])==0 and res[self.right_trigger][2]==0
+
+
+    def add_intersection_stack(self):
+        if (self.left_path):
+            self.maze_stack.append("L")
+        if (self.middle_path):
+            self.maze_stack.append("M")
+        if (self.right_path):
+            self.maze_stack.append("R")
+
+
+    #If Left or Right Path is seen  
+        if (self.left_path or self.right_path) and not self.intersection:
+            #Move forward for some distance before turning
+            if self.right_path:
+                self.go_forward(1)
+                self.turn_right()
+                
+            if self.left_path:
+                self.go_forward(1)
+                self.turn_left()
+
+        #If centered on tape go forward
+        elif self.centered_on_tape and not self.intersection:
+            self.go_forward(0.2)
+        
+
+    def run_loop(self):
+    
+        """continuously check map and move accordingly"""
+        
+        if not self.maze_stack:
             #If any 2 directions are sensed at once
             #Sensed: if red values are greater than 100. 0=blue,1=green, 2=red
-            if (intersection):
-                #Add all the sensed directions to the maze stack.
-                if (left_path):
-                    maze_stack.append("L")
-                if (middle_path):
-                    maze_stack.append("M")
-                if (right_path):
-                    maze_stack.append("R")
             
-            #If Left or Right Path is seen  
-            elif (left_path or right_path) and not intersection:
-                #Move forward for some distance before turning
-                if right_path:
-                    self.go_forward(1)
-                    self.turn_right
-                    
-                if left_path:
-                    self.go_forward(1)
-                    self.turn_left
-
-            #If centered on tape go forward
-            elif centered_on_tape and not intersection:
-                self.forward(0.2)
+            if (self.intersection):
+                #Add all the sensed directions to the maze stack.
+                self.add_intersection_stack()
+            
+            #else line follow
+            else:
+                self.line_follower()
         #If there's a maze stack begin investigations
-        if maze_stack:  
-            if not dead_end:
-                if maze_stack:
-                    if maze_stack.top("L"):
+        if self.maze_stack:  
+            if not self.dead_end:
+                if self.maze_stack:
+                    if self.maze_stack[-1] == "L":
                         self.turn_left()
-                    if maze_stack.top("R"):
+                    if self.maze_stack[-1] == "R":
                         self.turn_right()
-                    while not intersection:
-                        self.go_forward(1)
+                    while not self.intersection:
+                        self.line_follower()
+
+                    if self.intersection:
+                        self.maze_stack.append(",")
+                        self.add_intersection_stack()
+
+            if self.dead_end and self.maze_stack[-1]!=",":
+                if self.maze_stack[-1] == "L":
+                    self.turn_left()
+                    self.turn_left()
+                    while not self.intersection:
+                        self.line_follower()
+                    self.turn_left()
+                    self.maze_stack.pop()
+                elif self.maze_stack[-1] == "R":
+                    self.turn_right()
+                    self.turn_right()
+                    while not self.intersection:
+                        self.line_follower()
+                    self.turn_right()
+                    self.maze_stack.pop()
+                elif self.maze_stack[-1] == "M":
+                    self.turn_left()
+                    self.turn_left()
+                    while not self.intersection:
+                        self.line_follower()
+                    self.turn_left()
+                    self.turn_left()
+                    self.maze_stack.pop()
+            if self.dead_end and self.maze_stack[-1]==",":
+                self.turn_right()
+                self.turn_right()
+                while not self.intersection:
+                        self.line_follower()
+                self.maze_stack.pop()
+                if self.maze_stack[-1] == "R":
+                    self.turn_left()
+                    self.turn_left()
+                elif self.maze_stack[-1] == "L":
+                    self.turn_right()
+                    self.turn_right()
+                self.maze_stack.pop()        
         
 
         # get res as a normal numpy OpenCV image
